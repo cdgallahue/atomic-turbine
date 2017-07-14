2b7dd7e804cdbdc36b82e5150dbf02ac19a9089d
jdwyerge <john.dwyer1@ge.com>
2017-07-14 10:34:32 -0400

    added heatbeat

--- MVP.py
+++ MVP.py
@@ -25,11 +25,20 @@ def getVoltage(turbine):
     myValue = myArray[3].split('}')
     return float(myValue[0])
 
+def getHeartBeat(turbine):
+    url = 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/' + str(turbine) + '/heartbeat'
+    heartbeat = urllib2.urlopen(url).read()
+    myArray = heartbeat.split(':')
+    return myArray[1]
+
+def getTimeStamp(turbine):
+    return 1
+
 resp = requests.get('https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/temperature')
 while (1):
     #print voltage of each turbine
     for i in [1, 2, 3]:
-        print('turbine {0}: Voltage->{1} -- Temperature->{2}.'.format(i, getVoltage(i), getTemp(i)))
+        print('turbine {0}: Voltage->{1} -- Temperature->{2} -- Heartbeat->{3}'.format(i, getVoltage(i), getTemp(i), getHeartBeat(i)))
     ## wait 2 seconds before printing again
     time.sleep(2)
     resp = requests.get('https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api')
