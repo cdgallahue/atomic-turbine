b6b97ce9b67c77b651762fc7934d41b89ce64da0
Nicholas Smith <nsmith78@ufl.edu>
2017-07-19 16:47:34 -0400

    Updated Tabbed UI

--- web/homepage.py
+++ web/homepage.py
@@ -1,11 +1,13 @@
 import os
+from datetime import datetime
 from flask import Flask, make_response, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_indiv_data():
-    return render_template('indiv_turbine.html')
+	timestamp = str(datetime.now())
+    	return render_template('indiv_turbine.html', timestamp=timestamp)
 
 if __name__ == '__main__':
     app.run(debug=True)
