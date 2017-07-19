63ec98109946de11ba1154c5999330c1ca538a67
cdgallahue <cdgallahue@gmail.com>
2017-07-19 16:32:13 -0400

    added live jquery update of turbine status

--- web/homepage.py
+++ web/homepage.py
@@ -1,13 +1,29 @@
 import os
 from flask import Flask, make_response, render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
+@cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def show_indiv_data():
     return render_template('indiv_turbine.html')
 
+def add_headers(response):
+    """
+    Fix for font-awesome files: after Flask static send_file() does its
+    thing, but before the response is sent, add an
+    Access-Control-Allow-Origin: *
+    HTTP header to the response (otherwise browsers complain).
+    """
+
+    if (request.path):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 @app.route('/download')
+@cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def download():
 	return "Downloading"
 	
