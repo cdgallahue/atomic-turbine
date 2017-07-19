9a5f2333f879c30fa30c21b85de212b3c32cbd9c
Holly Haraguchi <holly.haraguchi@gmail.com>
2017-07-19 11:46:23 -0700

    Single dashboard

--- web/build.sh
+++ web/build.sh
@@ -2,10 +2,12 @@ rm -rf dist
 
 echo "Creating dist.."
 mkdir dist
+mkdir dist/templates/
 
 echo "Copying files.."
 cp ./*.py ./dist/
 cp ./requirements.txt ./dist/
 cp ./runtime.txt ./dist/
-
+cp ./templates/* ./dist/templates/
+cp Procfile ./dist/
 echo "Done."
--- web/dist/homepage.py
+++ web/dist/homepage.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def show_indiv_data():
-    return render_template('indiv_turbine.html')
-
+#    return render_template('indiv_turbine.html')
+    return "hello"
 @app.route('/download')
 def download():
 	return "Downloading"
 	
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
