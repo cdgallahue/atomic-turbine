9c31093e51b5e6c760864401124e2a9a5840b1be
cdgallahue <cdgallahue@gmail.com>
2017-07-19 12:56:47 -0400

    Added queries

--- UAA-turbine.py
+++ UAA-turbine.py
@@ -4,12 +4,12 @@ import os
 import websocket
 import time
 import urllib2
-import psycopg2
+#import psycopg2
 
 
 def get_uaa_header():
-	return 'Bearer ' + access_token
-	
+    return 'Bearer ' + access_token
+    
 #url = os.environ['UAA_URI']
 url = 'https://ff2359d6-05b4-4a0f-9001-2533c77cfe9d.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token'
 
@@ -17,7 +17,7 @@ payload = "grant_type=client_credentials"
 headers = {
     'content-type': "application/x-www-form-urlencoded",
 #    'authorization': os.environ['UAA_Authorization']
-	'authorization': 'Basic dHMtY2xpZW50MTpLZld1cHhTd001Q1hmaDg='
+    'authorization': 'Basic dHMtY2xpZW50MTpLZld1cHhTd001Q1hmaDg='
     }
 
 response     = requests.request("POST", url, data=payload, headers=headers)
@@ -36,67 +36,75 @@ query_url = 'https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/
 currTime = int(round(time.time()))
 
 def query_turbine_status(turbine_num):
-	tempUrl = 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/' + str(turbine_num) + '/heartbeat'
-	return requests.get(tempUrl).json()
-	
+    tempUrl = 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/' + str(turbine_num) + '/heartbeat'
+    return requests.get(tempUrl).json()
+    
 def query_turbine_temp(turbine_num):
-	tempUrl = 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/' + str(turbine_num) + '/sensors/temperature'
-	myArry = requests.get(tempUrl).json()	
-	return str(myArry["value"])
-	
+    tempUrl = 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/' + str(turbine_num) + '/sensors/temperature'
+    myArry = requests.get(tempUrl).json()   
+    return myArry
+    
 def query_turbine_volt(turbine_num):
-	tempUrl = 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/' + str(turbine_num) + '/sensors/voltage'
-	myArry = requests.get(tempUrl).json()	
-	return str(myArry["value"])
-	
+    tempUrl = 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/' + str(turbine_num) + '/sensors/voltage'
+    myArry = requests.get(tempUrl).json()   
+    return myArry
+    
 def post_data(timestamp, type, value, turbine_num):
-	tempData = [
-			[timestamp, value, 3],
-				]
-	headers = {
-		'Authorization': get_uaa_header(),
-		'Predix-Zone-Id': zone_id,
-		'Content-Type': 'application/json',
-			}
-	message = {
-		"messageId": 'atomic-' + type + '-' + str(timestamp),
-		"body": [
-				 {
-				 "name": 'atomic-turbine' + str(turbine_num) + '-' + type,
-				 "datapoints": tempData
-				 }
-				 ]
-			}
-	ws = websocket.create_connection(ingestion_url, header=headers)
-	ws.send(json.dumps(message))
-	return ws.recv()
+    tempData = [
+            [timestamp, value, 3],
+                ]
+    headers = {
+        'Authorization': get_uaa_header(),
+        'Predix-Zone-Id': zone_id,
+        'Content-Type': 'application/json',
+            }
+    message = {
+        "messageId": 'atomic-' + type + '-' + str(timestamp),
+        "body": [
+                 {
+                 "name": 'atomic-turbine' + str(turbine_num) + '-' + type,
+                 "datapoints": tempData
+                 }
+                 ]
+            }
+    ws = websocket.create_connection(ingestion_url, header=headers)
+    ws.send(json.dumps(message))
+    return ws.recv()
 
 #post data
 
 #try:
-	#conn = psycopg2.connect(dbname='energystream', user='johndwyer', host='localhost', password='')
-	#conn.autocommit = True
+    #conn = psycopg2.connect(dbname='energystream', user='johndwyer', host='localhost', password='')
+    #conn.autocommit = True
 #except:
-	#print("i could not connect")
-	#sys.exit()
+    #print("i could not connect")
+    #sys.exit()
 #cur = conn.cursor()
 
 while (1):
-	for turbine in [1, 2, 3]:
-		currTime = time.time()
-		status = query_turbine_status(turbine)["status"]
-		#print("Turbine " + str(turbine) + " status as of " + str(currTime) + ": " + status)
-		post_data(currTime, "status", status, turbine)
-		if status == "ONLINE":
-			volt = query_turbine_volt(turbine)#["voltage"]
-			temp = query_turbine_temp(turbine)#["temperature"]
-			post_data(currTime, "temp", temp, turbine)
-			post_data(currTime, "volt", volt, turbine)
-			print("Turbine: " + str(turbine) + " Time: " + str(currTime) + " Volts: " + str(volt) + " Temp: " + str(temp))
-			#insert = "INSERT INTO energystream (voltage, time, temp, heartbeat, turbineid) VALUES ('" + str(volt) + "', '" + str(currTime) + "', '" + str(temp) + "', '" + str(status) + "', " + str(turbine) + ");"
-        	#cur.execute(insert)
-	time.sleep(3)
-		
+    for turbine in [1, 2, 3]:
+        currTime = time.time()
+        status = query_turbine_status(turbine)["status"]
+        #print("Turbine " + str(turbine) + " status as of " + str(currTime) + ": " + status)
+        post_data(currTime, "status", status, turbine)
+        if status == "ONLINE":
+            volt = query_turbine_volt(turbine)#["voltage"]
+            temp = query_turbine_temp(turbine)#["temperature"]
+            if not type(temp) is None:
+                post_data(currTime, "temp", temp["value"], turbine)
+            else:
+                post_data(currTime, "temp", 0, turbine)
+            if not type(volt) is None:
+                post_data(currTime, "volt", volt["value"], turbine)
+            else:
+                post_data(currTime, "volt", 0, turbine)
+            print("Turbine: " + str(turbine) + " Time: " + str(currTime) + " Status: " + status)
+        else:
+            print("Turbine: " + str(turbine) + " Time: " + str(currTime) + " Status: " + status)
+            #insert = "INSERT INTO energystream (voltage, time, temp, heartbeat, turbineid) VALUES ('" + str(volt) + "', '" + str(currTime) + "', '" + str(temp) + "', '" + str(status) + "', " + str(turbine) + ");"
+            #cur.execute(insert)
+    time.sleep(3)
+        
 #print('Got back message confirmation TimeSeries:\n %s' % result)
 
 
